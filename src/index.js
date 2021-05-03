0cf1199ceb89bda1b3e15dc3633731fe26850e3e
jordyvirtuoso@gmail.com <jordyvirtuoso@gmail.com>
2021-05-03 18:37:24 -0400

    added bootstrap.min.css file and imported to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './bootstrap.min.css' // import bootstrap css file we downloaded
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
