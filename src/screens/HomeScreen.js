9efdb7d3e6bcfe11e2ca5be715fa276e1ace7cc9
jordyvirtuoso@gmail.com <jordyvirtuoso@gmail.com>
2021-05-03 19:11:57 -0400

    created screens folder for files displaying different views

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import products from '../products'
+
+function HomeScreen() {
+    return (
+        <div>
+            <h1>Latest Products</h1>
+        </div>
+    )
+}
+
+export default HomeScreen
