f9dc007966a3a71d40ea07eb1f6b09a5985e20de
jordyvirtuoso@gmail.com <jordyvirtuoso@gmail.com>
2021-05-03 19:40:21 -0400

    populated Cards with product images and added links by their product._id

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 
-function Product() {
+function Product({ product }) {
     return (
         <Card className="my-3 p-3 rounded">
-            Product
+            <a href={`/product/${product._id}`}>
+                <Card.Img src={product.image} />
+            </a>
         </Card>
     )
 }
