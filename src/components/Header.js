ea36fa165095ea603e4a87cea79d96d3c2f1ff84
jordyvirtuoso@gmail.com <jordyvirtuoso@gmail.com>
2021-05-03 18:58:08 -0400

    changed Navbar to dark, made it more responsive, and put into Container

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,17 @@ import { Navbar, Nav, Container, Row } from 'react-bootstrap'
 function Header() { // functioned was automatically named Header because of file name
     return (
         <header>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                    </Nav>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                        </Nav>
                 </Navbar.Collapse>
+                </Container>
             </Navbar>
         </header>
     )
