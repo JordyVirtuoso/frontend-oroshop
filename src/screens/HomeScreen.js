a6ea9a3cf6c9746ea1368b6d8bd0b46775b29ba9
jordyvirtuoso@gmail.com <jordyvirtuoso@gmail.com>
2021-05-03 19:35:30 -0400

    cols in HomeScreen.js now show product from Product.js, styled as Cards

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+
 import products from '../products'
 
 function HomeScreen() {
@@ -11,7 +13,7 @@ function HomeScreen() {
                     // coloumn size varies depending on display
                     // id of the product is added to key else error
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <h3>{product.name}</h3>
+                        <Product product={product} />
                     </Col>
                 ))}
             </Row>
