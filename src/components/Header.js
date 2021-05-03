d11b95d79353aa45a202099a1a27a976249545fe
jordyvirtuoso@gmail.com <jordyvirtuoso@gmail.com>
2021-05-03 18:52:52 -0400

    added bootstrap Navbar to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
 // use rfce to generate this component
 
 import React from 'react'
+import { Navbar, Nav, Container, Row } from 'react-bootstrap'
 
 function Header() { // functioned was automatically named Header because of file name
     return (
-        <div>
-            <header>Header</header>
-        </div>
+        <header>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </header>
     )
 }
 
