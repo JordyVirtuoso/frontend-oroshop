65fff73f15fba88ec22c6e05fa7ed3a091504365
jordyvirtuoso@gmail.com <jordyvirtuoso@gmail.com>
2021-05-03 18:19:05 -0400

    added fucntion in App.js to show Header.js and Footer.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+// used imp shortcut to generate skeleton that takes in Header and Footer
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App() {
   return (
     <div>
-      My App
+      <Header />
+      <main>
+        <h1>Welcome</h1>
+      </main>
+      <Footer />
     </div>
   );
 }
